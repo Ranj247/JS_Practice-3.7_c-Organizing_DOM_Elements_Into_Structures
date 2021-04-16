97396a17813fdf7ea30eb8201b5aa742639d4a16
Ranj247 <ranjeet.sadioura@gmail.com>
2021-04-16 18:55:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Get the body - create a variable tbody using tag and index location 
+// Get all the rows - create a variable rows  
+let 
+
+// Set up the array to hold our people
+
+
+// Iterate through all rows, "build" a person, and push them into the array
+
+
+// Voila!
+
+
+// Now you can do anything you want...find people who love steak!
+
+
+// Sort by age (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
+
+
+// Get the name of the youngest person
+
+
+// Or using something more complex, like Array.prototype.reduce()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
